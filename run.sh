2b7564dcefd656824e647b8a73c489f8c957436a
Monnot Stéphane <monnot.stephane@gmail.com>
2016-06-23 14:41:32 +0200

    Initial commit

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$PWD"
+echo "$PWD"
+
+if [ -f "./screenshot.conf" ]
+then
+    source "./screenshot.conf"
+else
+    echo "screenshot.conf: No such file."
+    exit 255
+fi
+
+FILENAME="$(tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1).png"
+
+gnome-screenshot -a -f "$DESTINATION/$FILENAME"
+
+echo "$URL/$FILENAME" | xclip -selection c
+
+
